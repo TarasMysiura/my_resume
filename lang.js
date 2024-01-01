85d0bf6f17dcaabad9b75af3d8f1a10dd0db491c
TarasMysiura <118620052+TarasMysiura@users.noreply.github.com>
2024-01-01 15:38:58 +0200

    add

---
+++ lang.js
@@ -0,0 +1,127 @@
+lang = {
+  "btn-primary": {
+    en: "Download CV",
+    ua: "Завантажити",
+  },
+  "resume-name": {
+    en: "Taras Mysiura",
+    ua: "Тарас Мисюра",
+  },
+  "resume-role-title": {
+    en: "Junior Full-Stack developer",
+    ua: "Тарас Мисюра",
+  },
+  "resume-section-heading": {
+    en: "About Me",
+    ua: "Про мене",
+  },
+  "resume-section-heading": {
+    en: "Projects",
+    ua: "Проєкти",
+  },
+  "item-heading": {
+    en: "Phonebook",
+    ua: "Телефонна кгнига",
+  },
+  "item-desc": {
+    en: "A website with a responsive I built a project on GitHub and used react-router-dom for navigation. Implemented registration and login form by using redux async and API. Saving and deleting contacts in the phonebook is also done with redux async and the same API.",
+    ua: "Я створив проект на GitHub і використав react-router-dom для навігації. Реалізована форма реєстрації та входу за допомогою асинхронного редуксування та API. Збереження та видалення контактів у телефонній книзі також виконується за допомогою redux async і того самого API.",
+  },
+  "item-heading": {
+    en: "Cinemania",
+    ua: "Кіноманія",
+  },
+  "item-desc": {
+    en: "This web app simplifies movie search, offering trailers and personalized libraries. HTML/CSS/JavaScript create a user-friendly interface. Axios/APIs enable real-time data. GitHub aids collaboration. Parcel Bundler optimizes performance. Explore, watch, and organize films effortlessly.",
+    ua: "Ця веб-програма спрощує пошук фільмів, пропонуючи трейлери та персоналізовані бібліотеки. HTML/CSS/JavaScript створюють зручний інтерфейс. Axios/API дозволяють отримувати дані в реальному часі. GitHub сприяє співпраці. Parcel Bundler оптимізує продуктивність. Досліджуйте, дивіться та впорядковуйте фільми без зусиль.",
+  },
+  "item-heading": {
+    en: "Mimiho",
+    ua: "Міміно",
+  },
+  "item-desc": {
+    en: "Web-site for a restaurant-hotel complex. The first team project was pretty simple, I enjoyed being working in a team with other people. I have some experience in this project. I was a team leader in this project.",
+    ua: "Сайт для ресторанно-готельного комплексу. Перший командний проект був досить простим, мені подобалося працювати в команді з іншими людьми. У мене є певний досвід у цьому проекті. Я був керівником команди в цьому проекті.",
+  },
+  "item-heading": {
+    en: "Webstudio",
+    ua: "Вебстудія",
+  },
+  "item-desc": {
+    en: "The first own project was quite simple. the first experience in IT technology was gained.",
+    ua: "Перший власний проект був досить простим. отримано перший досвід роботи в IT-технологіях.",
+  },
+  "resume-section-heading": {
+    en: "Work Experience",
+    ua: "Досвід роботи",
+  },
+  "resume-position-time": {
+    en: "2007 - Present",
+    ua: "2007 - Теперішній час",
+  },
+  "resume-company-name": {
+    en: "2007 - National university of food technologies | Kyiv",
+    ua: "2007 - Національний університет харчових технологій м. Київ",
+  },
+  "resume-position-title mb-1": {
+    en: "Associate Professor of the Department of Food Production Processes and Apparatus",
+    ua: "Доцент кафедри процесів та апаратів харчових виробництв",
+  },
+  "resume-timeline-list-item": {
+    en: "Conducting lecture, laboratory and practical classes in accordance with the curriculum, directing the course design of the training of applicants at the university and its branches.",
+    ua: "Проведення лекційних, лабораторних та практичних занять відповідно до навчального плану, керівництво курсовим проектуванням підготовки здобувачів в університеті та його філіях.",
+  },
+  "resume-timeline-list-item": {
+    en: "Management of independent and scientific work of full-time and part-time students.",
+    ua: "2007 - Керівництво самостійною та науковою роботою студентів денної та заочної форми навчання.",
+  },
+  "resume-timeline-list-item": {
+    en: "Execution and management of departmental research work and fundamental and applied research work on state budget topics, performance of duties.",
+    ua: "Виконання та керівництво кафедральною науково-дослідною роботою та фундаментально-прикладними науково-дослідними роботами з держбюджетної тематики, виконання обов'язків.",
+  },
+  "resume-timeline-list-item": {
+    en: "Preparation of educational and methodological, visual and electronic materials for disciplines.",
+    ua: "Підготовка навчально-методичних, наочних та електронних матеріалів до дисциплін.",
+  },
+  "resume-timeline-list-item": {
+    en: "I have 310 publications, 63 articles in specialized scientific publications, 17 in international publications, 9 included in the SCOPUS and Web of Science Core Collection scientometric databases; 37 patents (23 for utility model, 14 for invention), 1 textbook and 3 training manuals.",
+    ua: "Маю 310 публікацій, 63 статті у спеціалізованих наукових виданнях, 17 у міжнародних виданнях, 9 включено до наукометричних баз даних SCOPUS та Web of Science Core Collection; 37 патентів (23 на корисну модель, 14 на винахід), 1 підручник та 3 навчальних посібники.",
+  },
+  "resume-position-time": {
+    en: "2021 - Present",
+    ua: "2021 - Теперішній час",
+  },
+  "resume-company-name": {
+    en: "Specialized school of I-III degrees with in-depth study of the English language No. 92 named after Ivan Franko of the city of Kyiv | Kyiv.",
+    ua: "Спеціалізована школа І-ІІІ ступенів з поглибленим вивченням англійської мови № 92 імені Івана Франка міста Києва | Київ.",
+  },
+  "resume-position-title mb-1": {
+    en: "Informatics teacher",
+    ua: "Вчитель інформатики",
+  },
+  "resume-timeline-list-item": {
+    en: "Conducting computer science lessons in 7th, 8th, 9th and 10th grades according to the approved plan.",
+    ua: "Проведення уроків інформатики у 7, 8, 9 та 10 класах за затвердженим планом.",
+  },
+  "resume-position-time": {
+    en: "2023 - Present",
+    ua: "2023 - Теперішній час",
+  },
+  "resume-company-name": {
+    en: "Specialized school of I-III degrees with in-depth study of the English language No. 92 named after Ivan Franko of the city of Kyiv | Kyiv.",
+    ua: "Спеціалізована школа І-ІІІ ступенів з поглибленим вивченням англійської мови № 92 імені Івана Франка міста Києва | Київ.",
+  },
+  "resume-position-title mb-1": {
+    en: "Physics teacher",
+    ua: "Вчитель фізики",
+  },
+  "resume-timeline-list-item": {
+    en: "Conducting physics lessons in 7th, 8th, 9th and 10th grades caccording to the approved plan.",
+    ua: "Проведення уроків фізики у 7, 8, 9 та 10 класах згідно затвердженого плану.",
+  },
+
+  "resume-name": {
+    en: "Work Experience",
+    ua: "Тарас Мисюра",
+  },
+};
